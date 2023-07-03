5287f1e14089668f130a6135eaae142fcc6cadf6
Katerina <smilevskakaterina5@gmail.com>
2023-07-03 03:10:34 +0200

    SQLite database.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -85,18 +85,17 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MusicStore',
-        'USER': 'postgres',
-        'PASSWORD': 'p0stgr3',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'MusicStore',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'p0stgr3',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
 }
 
 # Password validation
